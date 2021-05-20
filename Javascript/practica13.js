36454f97dbfaeda48e3252a9a09eb0ecc3cfe722
Francisco Manuel Molina <info@franciscomanuelmolina.com>
2021-05-20 18:40:24 +0200

    Add files via upload

---
+++ Javascript/practica13.js
@@ -0,0 +1,4 @@
+const valor = prompt("Introduce un número");
+const resultado = valor < 0;
+
+console.log("Es negativo?", resultado);
---
+++ Javascript/practica14.js
@@ -0,0 +1,4 @@
+const valor = prompt("Introduce un número");
+const resultado = (valor >= 0) && (valor % 2 === 0);
+
+console.log("Es par y positivo?", resultado);
