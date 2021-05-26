28fecda423bd20ac9012239e290b3e7fcca0f32b
Francisco Manuel Molina <info@franciscomanuelmolina.com>
2021-05-26 20:39:36 +0200

    Add files via upload

---
+++ Javascript/ejercicio3-1.js
@@ -0,0 +1,9 @@
+let valor = prompt("Introduce un numero");
+
+if (valor > 0) {
+    for (let i = 0; i < valor; i++) {
+        console.log("Hola");
+    }
+} else {
+    console.log("El valor introducido no es mayor que 0");
+}
